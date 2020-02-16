40f8b9d34716d2bb5e79f1fbd321b7ced21b115a
Tharesh <99tharesh@gmail.com>
2020-02-16 18:00:11 +0530

    Added stocks sort by date

--- src/redux/reducers/stocksReducer.js
+++ src/redux/reducers/stocksReducer.js
@@ -17,8 +17,19 @@ const stocksReducer = (state = initialState, action) => {
       });
     case "UPDATE_STOCK_DATA":
       let newStock = action.stock;
+      let updatedStocksList = [
+        { id: newStock.id, ...newStock.fields },
+        ...state.stocks
+      ];
+      updatedStocksList.sort((stockA, stockB) => {
+        let stockADate = new Date(stockA.date);
+        let stockBDate = new Date(stockB.date);
+        if (stockADate < stockBDate) return -1;
+        if (stockADate > stockBDate) return 1;
+        return 0;
+      });
       return Object.assign({}, state, {
-        stocks: [...state.stocks, { id: newStock.id, ...newStock.fields }]
+        stocks: updatedStocksList
       });
     case "DELETE_STOCK_DATA":
       let stocksInState = state.stocks;
