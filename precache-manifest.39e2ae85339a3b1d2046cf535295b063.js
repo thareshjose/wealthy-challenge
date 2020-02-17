65539cd81762195a8969feaf76787af54c2f5d70
Tharesh <99tharesh@gmail.com>
2020-02-17 08:31:13 +0530

    Updates

---
+++ precache-manifest.39e2ae85339a3b1d2046cf535295b063.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b4d9c9dc417d29ac3bf4202bc6532e9",
+    "url": "/wealthy-challenge/index.html"
+  },
+  {
+    "revision": "7a3488d06fa5374a335c",
+    "url": "/wealthy-challenge/static/css/2.9ddb28ea.chunk.css"
+  },
+  {
+    "revision": "bda24f738233730bd65c",
+    "url": "/wealthy-challenge/static/css/main.39822954.chunk.css"
+  },
+  {
+    "revision": "7a3488d06fa5374a335c",
+    "url": "/wealthy-challenge/static/js/2.dbb26e04.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/wealthy-challenge/static/js/2.dbb26e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda24f738233730bd65c",
+    "url": "/wealthy-challenge/static/js/main.2455c1a4.chunk.js"
+  },
+  {
+    "revision": "8c8197b88663b40ec7e8",
+    "url": "/wealthy-challenge/static/js/runtime-main.889c1f11.js"
+  }
+]);
